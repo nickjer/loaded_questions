e3c7679f6f10a935cd294ac0855d9911d2feccf3
Jeremy Nicklas <jeremywnicklas@gmail.com>
2021-10-23 17:40:10 -0400

    validate single root round per game

--- app/models/round.rb
+++ app/models/round.rb
@@ -18,12 +18,19 @@ class Round < ApplicationRecord
   validates :question, presence: true
   validates :previous, uniqueness: { allow_blank: true }
   validate :all_rounds_completed, on: :create
+  validate :single_root_round, on: :create
 
   private
 
   def all_rounds_completed
-    return unless game.rounds.status_active.exists?
+    return unless player.game.rounds.status_active.exists?
 
     errors.add(:base, "Active round currently exists for this game")
+  end
+
+  def single_root_round
+    return if previous.present? || !player.game.rounds.exists?
+
+    errors.add(:base, "Game already has an initial round")
   end
 end
