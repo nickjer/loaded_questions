31bb35ab03e1614bc8917b0bc9b9258520966bd2
Jeremy Nicklas <jeremywnicklas@gmail.com>
2021-11-02 20:04:54 -0400

    support a stream for the game

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 
   # @return [String, nil]
   def current_user_id
-    cookies.permanent.encrypted[:current_user_id].to_s.presence
+    cookies.encrypted[:current_user_id].to_s.presence
   end
 
   # @return [void]
