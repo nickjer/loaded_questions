d2a39c017d06cefc0eddab3e3390d47e9a9e6173
Jeremy Nicklas <jeremywnicklas@gmail.com>
2021-11-06 00:10:54 -0400

    add custom meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def display_meta_tags
+    content_for(:meta_tags)
+  end
+
+  def add_meta_tags(**meta_tags)
+    content_for :meta_tags do
+      meta_tags.each do |name, content|
+        concat(tag.meta(name: name, content: content))
+      end
+    end
+  end
 end
