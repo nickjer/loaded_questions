187a5382911483363da089d2c28e3bb55e621ec1
Jeremy Nicklas <jeremywnicklas@gmail.com>
2021-11-14 19:31:19 -0500

    fix weird issue with debug gem

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,8 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  # See https://github.com/ruby/debug/issues/336
+  DEBUGGER__::CONFIG[:fork_mode] = :parent
+
   driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
 end
