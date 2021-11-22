aa4ae59f6941c9850174d8202f4bd6ee4505aa9e
Jeremy Nicklas <jeremywnicklas@gmail.com>
2021-11-22 16:41:22 -0500

    clean up migrations

--- db/migrate/20211016162212_create_rounds.rb
+++ db/migrate/20211016162212_create_rounds.rb
@@ -4,13 +4,12 @@ class CreateRounds < ActiveRecord::Migration[7.0]
   def change
     create_table :rounds, id: :uuid do |t|
       t.references :player, type: :uuid, null: false, foreign_key: true
-      t.references :previous, type: :uuid, index: { unique: true }
+      t.references :previous, type: :uuid, index: { unique: true },
+        foreign_key: { to_table: :rounds }
       t.integer :status, default: 0, null: false
       t.text :question
 
       t.timestamps
-
-      t.foreign_key :rounds, column: :previous_id
     end
   end
 end
--- db/migrate/20211019215845_create_answers.rb
+++ db/migrate/20211019215845_create_answers.rb
@@ -6,14 +6,13 @@ class CreateAnswers < ActiveRecord::Migration[7.0]
       t.text :value, null: false
       t.references :player, type: :uuid, null: false, foreign_key: true
       t.references :round, type: :uuid, null: false, foreign_key: true
-      t.references :guessed_player, type: :uuid
+      t.references :guessed_player, type: :uuid, index: true,
+        foreign_key: { to_table: :players }
 
       t.timestamps
 
       t.index %i[player_id round_id], unique: true
       t.index %i[guessed_player_id round_id], unique: true
-
-      t.foreign_key :players, column: :guessed_player_id
     end
   end
 end
